4ff065805dddc6fe00975becf138bba0fa033444
Biggs, Brandon Samuel <brandon.biggs@inl.gov>
2022-04-09 16:03:50 -0600

    fix

--- pbs_chimp2_theme.bash
+++ pbs_chimp2_theme.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -N $1
+#PBS -N $job_name
 #PBS -l select=1:ncpus=1:mem=8gb
 #PBS -q router
 #PBS -P edu_res
@@ -9,4 +9,4 @@
 module load conda
 conda activate chimp
 cd /home/biggbs/school/Chimp
-python3 $2
+python3 $file
--- submit_themes.bash
+++ submit_themes.bash
@@ -1,5 +1,7 @@
 #/bin/bash
-qsub ./pbs_chimp2_theme.bash chimp2-flower chimp2-theme-flower.py
+$job_name=chimp2-flower
+$file=chimp2-theme-flower.py
+qsub ./pbs_chimp2_theme.bash
 #./pbs_chimp2_theme.bash chimp2-lake chimp2-theme-lake.py
 #./pbs_chimp2_theme.bash chimp2-mountain chimp2-theme-mountain.py
 #./pbs_chimp2_theme.bash chimp2-puppy chimp2-theme-puppy.py
