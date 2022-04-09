8fa09002f0179502fb693b718b265cf0114f746e
Biggs, Brandon Samuel <brandon.biggs@inl.gov>
2022-04-09 16:00:29 -0600

    fix

--- submit_themes.bash
+++ submit_themes.bash
@@ -1,5 +1,5 @@
 #/bin/bash
-./pbs_chimp2_theme.bash chimp2-flower chimp2-theme-flower.py
+bash ./pbs_chimp2_theme.bash chimp2-flower chimp2-theme-flower.py
 #./pbs_chimp2_theme.bash chimp2-lake chimp2-theme-lake.py
 #./pbs_chimp2_theme.bash chimp2-mountain chimp2-theme-mountain.py
 #./pbs_chimp2_theme.bash chimp2-puppy chimp2-theme-puppy.py
