20757932afa7702e3504267f8d67bfbe36d44ba2
Biggs, Brandon Samuel <brandon.biggs@inl.gov>
2022-04-23 12:31:35 -0600

    updated organize

--- organize.bash
+++ organize.bash
@@ -2,6 +2,10 @@ themes=("flower" "lake" "mountain" "puppy" "rain")
 for theme in ${themes[@]}; do
     awk 'NF' logs/chimp1-themes-$theme.txt | sort -k 9 >> results/chimp1.txt
     echo "" >> results/chimp1.txt
+
     awk 'NF' logs/chimp2-themes-$theme.txt | sort -k 9 >> results/chimp2.txt
+    echo "" >> results/chimp2.txt
+
     awk 'NF' logs/comp-themes-$theme.txt | sort -k 8 >> results/comp.txt
+    echo "" >> results/comp.txt
 done
